8f1e95a9e859ed4b7cac8f10e7809f3b8eb33b00
RBCampus <regisseki@gmail.com>
2016-12-05 19:10:11 -0200

    Config pt-BR locale

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Hope2us
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :"pt-BR"
+    config.time_zone = 'Brasilia'
   end
 end
