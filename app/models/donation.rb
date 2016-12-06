5cb98bd9abb7b2c008a278f1f699ff7d7baa4ec7
gitdbits <dbs.noronha@gmail.com>
2016-12-06 21:57:23 -0200

    WIP - donation validation

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,11 @@
 class Donation < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  validates :user, :item, presence: true
+  validate :limit_amount
+
+  def limit_amount
+  	"Todos os(as) #{item.name} foram doados" if item.amount == 0
+  end
 end
