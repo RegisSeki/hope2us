dbd55d2a217a4c6cb3c5ff3fe37528540533d0c4
RBCampus <regisseki@gmail.com>
2016-12-07 19:43:59 -0200

    Create validation of available item in donation model

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Donation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'unavailable item' do
+    it 'should not save with unavailable item' do
+      item = create(:item, amount: 0)
+      user = create(:user)
+      donation = build(:donation, user: user, item: item, amount: 2)
+
+      expect(donation.valid?).to be_falsey
+    end
+  end
 end
