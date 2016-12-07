68298bc6c135d450bd968ca99a310ffd5086d27c
gitdbits <dbs.noronha@gmail.com>
2016-12-07 20:00:51 -0200

    refact donation mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
- 	default from: 'notifications@example.com'
+  default from: 'notifications@example.com'
 
- 	def donation_mailer(user, donations)
- 		@donations = donations
- 		@user = user
- 		@url = 'Definir URL'
- 		mail(to: @user.email, subject: 'Obrigado! Hope2us')
- 	end
+  def donation_mailer(user, donations)
+    @donations = donations
+    @user = user
+    @url = 'Definir URL'
+    mail(to: @user.email, subject: 'Obrigado! Hope2us')
+  end
 end
--- spec/mailers/previews/donation_confirmation_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/donation_confirmation
-class DonationConfirmationPreview < ActionMailer::Preview
-
-end
