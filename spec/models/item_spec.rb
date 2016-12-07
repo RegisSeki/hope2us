00b7b277df1db5f4b6072035a75e8145a8064eb8
RBCampus <regisseki@gmail.com>
2016-12-07 20:52:04 -0200

    Create available method for Item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe Item, type: :model do
       expect(item.available_amounts).to eq([5, 4, 3, 2, 1])
     end
   end
+
+  describe 'available items' do
+    it 'should return list of available item' do
+      item_one = create(:item, amount: 0)
+      item_two = create(:item, amount: 5)
+
+      expect(Item.available.size).to eq(1)
+    end
+  end
 end
