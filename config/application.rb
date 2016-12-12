48f67958d85ef1ca557a4c38493ce4edd8985710
gitdbits <dbs.noronha@gmail.com>
2016-12-12 19:36:35 -0200

    Add fonts path to application

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Hope2us
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     config.autoload_paths += %W(#{config.root}/services)
-    
+
+    config.time_zone = 'Brasilia'
+
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :"pt-BR"
-    config.time_zone = 'Brasilia'
   end
 end
