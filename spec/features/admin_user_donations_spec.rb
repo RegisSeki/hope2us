ff3b6cf32980c9bf7150b6fb8beac7c78cade4e0
gitdbits <dbs.noronha@gmail.com>
2016-12-13 18:47:36 -0200

    wip admin_user_see_donations

---
+++ spec/features/admin_user_donations_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'admin user see donations'	do
+  scenario 'when logged ins' do
+    admin_user = create(:admin_user)
+    donation = create(:donation)
+
+    login_as(admin_user, scope: :admin_user)
+
+    visit admin_donations_path
+
+    expect(page).to have_content donation.id
+    expect(page).to have_content donation.item.name
+    expect(page).to have_content donation.amount
+    expect(page).to have_content donation.status
+  end
+end
