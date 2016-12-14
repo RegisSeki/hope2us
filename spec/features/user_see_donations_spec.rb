b2c31fbb56dd294c2ea27666d9bad1a30cbd4090
RBCampus <regisseki@gmail.com>
2016-12-14 19:38:42 -0200

    Improve cancel donation and see donation tests

--- spec/features/user_see_donations_spec.rb
+++ spec/features/user_see_donations_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'user see donations' do
   scenario 'when logged in' do
-    donation = create(:donation)
+    donation = create(:donation, image: nil, status: 'pending_receipt')
     another_user = create(:user, email: 'outro@email.com')
     another_donation = create(:donation, user: another_user)
 
@@ -13,6 +13,9 @@ feature 'user see donations' do
     click_on 'Minhas Doações'
 
     expect(page).to have_content donation.item.name
+    expect(page).to have_link(
+      'Enviar comprovante', href: edit_receipt_donation_path(donation)
+    )
     expect(page).not_to have_content another_donation.item.name
   end
 
